1bc3e6bfc84401620785f32b9c1ca6fa445b5dce
Bhavya Verma <vbhavya269@gmail.com>
2022-07-04 20:24:13 +0530

    💄 ⚡️ Added Flag and Text on the Header Top

--- src/components/HeaderTop/HeaderTop.js
+++ src/components/HeaderTop/HeaderTop.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./headerTop.css";
+import flag from "../../assets/images/india-flag.png";
 
 const HeaderTop = () => {
     return (
         <div className="flex-container">
-            <div className="flex-left" style={{flexGrow: '1'}}></div>
+            <div className="flex-left" style={{ flexGrow: '1' }}>
+                <img className="flag" src={flag} alt="Indian Flag"></img>
+                <div className="goi-text">
+                    भारत सरकार |  Government of India 
+                </div>
+            </div>
             <div className="flex-right" style={{ flexGrow: '1' }}></div>
         </div>
     )
