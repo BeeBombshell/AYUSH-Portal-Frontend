7f2f3fef8363a4107e4e6925cf3faa02a7127ba3
Bhavya Verma <vbhavya269@gmail.com>
2022-07-05 12:39:57 +0530

    🎨 💄 Added elements in Header Top

--- src/components/HeaderTop/HeaderTop.js
+++ src/components/HeaderTop/HeaderTop.js
@@ -5,10 +5,17 @@ import flag from "../../assets/images/india-flag.png";
 const HeaderTop = () => {
     return (
         <div className="flex-container">
-            <div className="flex-left" style={{ flexGrow: '1' }}>
-                <img className="flag" src={flag} alt="Indian Flag"></img>
-                <div className="goi-text">
-                    भारत सरकार |  Government of India 
+            <div className="flex-left">
+                <div className="wrapper-div">
+                    <img className="flag" src={flag} alt="Indian Flag"></img>
+                    <div className="goi-text">
+                        भारत सरकार |  Government of India
+                    </div>
+                    <button type="button" className="btn btn-outline-light">
+                        Regions
+                        &nbsp;
+                        <i className="fas fa-angle-down"></i>
+                    </button>
                 </div>
             </div>
             <div className="flex-right" style={{ flexGrow: '1' }}></div>
