1d6c6944920ecc82585872cdf21c015042457307
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-13 12:36:47 +0530

    💄⚡️ Styling for the HomeMid component

--- src/components/HomeMid/HomeMid.js
+++ src/components/HomeMid/HomeMid.js
@@ -13,6 +13,7 @@ function HomeMid() {
                 <div className="homeMid-textBlock">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Venenatis sed dignissim nam quam sed amet ultrices. Quisque nunc in pellentesque sociis amet quam mauris pulvinar. Eget diam id quis nam posuere sem quis rhoncus imperdiet.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Venenatis sed dignissim nam quam sed amet ultrices. Quisque nunc in pellentesque sociis amet quam mauris pulvinar. 
                 </div>
+                <div type="button" className="homeMid-button">Read More</div>
                 <img className="homeMid-imgBlock" src={ayurveda} alt="HomeMid-ImageBlock"></img>
             </div>
         </div>
